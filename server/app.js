a97504aebf30bb66367732db170aa779da506bec
d-kang <david.kang714@gmail.com>
2018-02-07 09:20:39 -0800

    Add body-parser to app

--- server/app.js
+++ server/app.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 const routes = require('./routes/index');
-
 const app = module.exports = express();
 const utils = require('./utils');
 
@@ -13,6 +13,8 @@ app.set('view engine', 'pug');
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
