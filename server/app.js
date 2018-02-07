2712e30150d872df8863caa4bc38640f8d2b7f62
d-kang <david.kang714@gmail.com>
2018-02-06 17:41:31 -0800

    Add Pug as view engine

--- server/app.js
+++ server/app.js
@@ -1,7 +1,16 @@
 'use strict';
 const express = require('express');
+const path = require('path');
 const routes = require('./routes/index');
-
 const app = module.exports = express();
+const helpers = require('./helpers');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use((req, res, next) => {
+  res.locals.h = helpers;
+  next();
+});
 
 app.use('/', routes);
