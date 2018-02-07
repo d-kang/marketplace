edd5b7f430dd29d6d094f99b8064a4d2b06243ac
d-kang <david.kang714@gmail.com>
2018-02-06 20:04:16 -0800

    Serve static html and add cookie parser middleware

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 'use strict';
+
 const express = require('express');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 const routes = require('./routes/index');
+
 const app = module.exports = express();
 const helpers = require('./helpers');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(cookieParser());
 
 app.use((req, res, next) => {
   res.locals.h = helpers;
