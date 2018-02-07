bf5017118ce5ba7986a2489da52f0639533274e4
d-kang <david.kang714@gmail.com>
2018-02-07 09:21:15 -0800

    Add dotenv, mongoose connection and singleton model to index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,16 @@
 'use strict';
-const app = require('./app');
+const mongoose = require('mongoose');
 
+
+require('dotenv').config({ path: 'variables.env'});
+mongoose.connect(process.env.DATABASE);
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', (error) => {
+  console.error('mongooes connection error', error);
+});
+
+require('./models/Item');
+const app = require('./app');
 app.set('port', process.env.PORT || 1337);
 
 const PORT = app.get('port');
