564c3ba72a2f8d9438c7cb14c4a75f9095dc0e8a
d-kang <david.kang714@gmail.com>
2018-02-07 09:24:09 -0800

    Add menu array to utils/index.js

--- server/utils/index.js
+++ server/utils/index.js
@@ -4,3 +4,30 @@ exports.moment = require('moment');
 
 exports.siteName = 'marketplace';
 
+exports.menu = [
+  {
+    slug: '/items',
+    title: 'Items',
+    icon: 'items',
+  },
+  {
+    slug: '/tags',
+    title: 'Tags',
+    icon: 'tag',
+  },
+  {
+    slug: '/top',
+    title: 'Top',
+    icon: 'top',
+  },
+  {
+    slug: '/add',
+    title: 'Add',
+    icon: 'add',
+  },
+  {
+    slug: '/map ',
+    title: 'Map',
+    icon: 'map',
+  },
+];
